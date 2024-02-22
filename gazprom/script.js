8ce53eb7252d03b01ad0f5255340e70a4007e88d
Snack <nurd_galim@mail.ru>
2024-02-22 23:45:45 +0700

    modal fix

--- gazprom/script.js
+++ gazprom/script.js
@@ -1,27 +1,3 @@
 document.getElementById("closeBth").addEventListener("click",function() {
     document.getElementById("header-scammer").style.display ="none";
 });
-
-// Получаем элементы модального окна, кнопку для его открытия и элемент закрытия
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("anket");
-var span = document.getElementsByClassName("close")[0];
-
-// Открываем модальное окно при клике на кнопку
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// Закрываем модальное окно при клике на крестик
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Закрываем модальное окно при клике вне его области
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
---
+++ gazprom/vacans.js
@@ -0,0 +1,23 @@
+window.onload = () => {
+
+  var modal = document.getElementById("myModal");
+  var btn = document.getElementById("anket");
+  var span = document.getElementsByClassName("close")[0];
+
+  // Открываем модальное окно при клике на кнопку
+  btn.onclick = function () {
+    modal.style.display = "block";
+  }
+
+  // Закрываем модальное окно при клике на крестик
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // Закрываем модальное окно при клике вне его области
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
